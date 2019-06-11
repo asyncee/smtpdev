3bba370ac5089430ae1013418bafdcfb1ea1e8b2
asyncee <asyncee@users.noreply.github.com>
2019-06-11 13:05:44 +0300

    Version 0.1.3

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ for dirpath, dirnames, filenames in os.walk("smtpdev"):
 
 setup(
     name="smtpdev",
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages(),
     author="asyncee",
     description=description,
