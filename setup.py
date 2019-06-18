c5f50300d53fbc842695a53b175f92a2d13438fe
Stanislav Lobanov <n10101010@gmail.com>
2019-06-18 16:12:43 +0300

    Version 0.2.1

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ for dirpath, dirnames, filenames in os.walk("smtpdev"):
 
 setup(
     name="smtpdev",
-    version="0.2",
+    version="0.2.1",
     packages=find_packages(),
     author="asyncee",
     description=description,
